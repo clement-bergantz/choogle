3bb57c76032935a0b6521d1427379847a710ab72
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-21 18:14:44 +0200

    Created view for proposals#new

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,2 +1,21 @@
 class ProposalsController < ApplicationController
+
+  def new
+    @choogle = Choogle.find(params[:choogle_id])
+    @proposal = Proposal.new
+  end
+
+  def create
+    @proposal = Proposal.new(proposal_params)
+    @proposal.choogle = Choogle.find(params[:choogle_id])
+    @proposal.save
+
+    redirect_to choogle_path(params[:choogle_id])
+  end
+
+  private
+
+  def proposal_params
+    params.require(:proposal).permit()
+  end
 end
