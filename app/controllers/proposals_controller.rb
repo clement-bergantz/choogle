8e5b13fa5c3dcdc37b08e160f9207e56dae94eb6
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-22 11:40:55 +0200

    wip on controller

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -7,6 +7,12 @@ class ProposalsController < ApplicationController
 
   def create
     @proposal = Proposal.new(proposal_params)
+    if Place.find_by(params[:place]).nil?
+      @place = Place.find_by(params(:place))
+    else
+      @place = Place.create.new(name: params[:place])
+    end
+    @proposal.place = @place
     @proposal.choogle = Choogle.find(params[:choogle_id])
     @proposal.save
 
