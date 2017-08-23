94bda516e3be464ee9c4bbf4633eaeca9eb2c8b1
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-23 15:25:12 +0200

    asking name (first_name) at signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+      # For additional fields in app/views/devise/registrations/new.html.erb
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    end
 end
