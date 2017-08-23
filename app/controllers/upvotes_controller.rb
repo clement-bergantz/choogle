e7adddffb0d1aae90c6a56fb5a2cecc918fa8b90
Romain Neuville <neuvilleromain@gmail.com>
2017-08-23 22:09:36 +0200

    downvotes done

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -3,20 +3,38 @@ class UpvotesController < ApplicationController
   def create
 
     @proposal = Proposal.find(params[:id])
-    @upvote = current_user.upvotes.new(proposal: @proposal)
     @choogle = Choogle.find_by_slug(params[:slug])
-    if @upvote.save
+
+    # Searching in DB if the upvote for this user and proposal exist, if not,
+    if current_user.upvotes.find_by(proposal: @proposal).nil?
+      @upvote = current_or_guest_user.upvotes.new(proposal: @proposal)
+      # Saving it and respond to Ajax request in JS, else render error in console with create.js.erb
+      if @upvote.save
         respond_to do |format|
           format.html { redirect_to choogle_path(@choogle) }
           format.js  # <-- will render `app/views/upvotes/create.js.erb`
         end
-    else
-      # Flash message to alert user if he already vote
-      # flash[:upvote] = 'You already votes for this proposal !'
+      else
         respond_to do |format|
           format.html { redirect_to choogle_path(@choogle) }
           format.js  # <-- idem
         end
+      end
+    # Searching in DB if the upvote for this user and proposal exist,
+    else current_user.upvotes.find_by(proposal: @proposal)
+      @upvote = current_or_guest_user.upvotes.find_by(proposal: @proposal)
+      # Destroying it and respond to Ajax request in JS, else render error in console with create.js.erb
+      if @upvote.destroy
+        respond_to do |format|
+          format.html { redirect_to choogle_path(@choogle) }
+          format.js  # <-- will render `app/views/upvotes/create.js.erb`
+        end
+      else
+        respond_to do |format|
+          format.html { redirect_to choogle_path(@choogle) }
+          format.js  # <-- idem
+        end
+      end
     end
   end
 
