efb490304a4b0f2e46b1536feecd3ba441f3ef6b
Romain Neuville <neuvilleromain@gmail.com>
2017-08-23 18:42:26 +0200

    changes on upvote with ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  # config.action_view.embed_authenticity_token_in_remote_forms = true
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   add_flash_types :upvote
