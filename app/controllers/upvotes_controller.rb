631cc56ad8997c4fb5bff3e846ef27e739462d22
Romain Neuville <neuvilleromain@gmail.com>
2017-08-24 11:21:17 +0200

    downvotes ok

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -9,33 +9,20 @@ class UpvotesController < ApplicationController
     if current_user.upvotes.find_by(proposal: @proposal).nil?
       @upvote = current_or_guest_user.upvotes.new(proposal: @proposal)
       # Saving it and respond to Ajax request in JS, else render error in console with create.js.erb
-      if @upvote.save
-        respond_to do |format|
-          format.html { redirect_to choogle_path(@choogle) }
-          format.js  # <-- will render `app/views/upvotes/create.js.erb`
-        end
-      else
-        respond_to do |format|
-          format.html { redirect_to choogle_path(@choogle) }
-          format.js  # <-- idem
-        end
+      @upvote.save
+      respond_to do |format|
+        format.html { redirect_to choogle_path(@choogle) }
+        format.js  # <-- will render `app/views/upvotes/create.js.erb`
       end
     # Searching in DB if the upvote for this user and proposal exist,
     else current_user.upvotes.find_by(proposal: @proposal)
       @upvote = current_or_guest_user.upvotes.find_by(proposal: @proposal)
       # Destroying it and respond to Ajax request in JS, else render error in console with create.js.erb
-      if @upvote.destroy
-        respond_to do |format|
-          format.html { redirect_to choogle_path(@choogle) }
-          format.js  # <-- will render `app/views/upvotes/create.js.erb`
-        end
-      else
-        respond_to do |format|
-          format.html { redirect_to choogle_path(@choogle) }
-          format.js  # <-- idem
-        end
+      @upvote.destroy
+      respond_to do |format|
+        format.html { redirect_to choogle_path(@choogle) }
+        format.js  # <-- will render `app/views/upvotes/create.js.erb`
       end
     end
   end
-
 end
