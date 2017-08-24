0cc362ec21486684c7c61b988b8d10751c2342e6
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-24 20:57:20 +0200

    user can now comment on a choogle

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,17 @@ class CommentsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:new, :create]
 
   def create
-    @comment = Comment.new
+    @comment = Comment.new(comment_params)
     @comment.user = current_or_guest_user
-    binding.pry
+    choogle = Choogle.find_by(slug: params[:slug])
+    @comment.choogle = choogle
     @comment.save
+    redirect_to choogle_path(choogle)
+  end
+
+  private
+
+  def comment_params
+      params.require(:comment).permit(:content)
   end
 end
