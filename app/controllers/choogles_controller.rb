e8ee9119ba6f0b8ed17a8bf054e905e6320c274d
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-24 14:16:50 +0200

    modals chaining working

--- app/controllers/choogles_controller.rb
+++ app/controllers/choogles_controller.rb
@@ -29,21 +29,36 @@ class ChooglesController < ApplicationController
 
   def create
     @user = current_user
-    @choogle = @user.choogles.new(choogle_params)
-
-    # we generate a random slug
-    slug = SecureRandom.urlsafe_base64(5)
-    # we check if the slug is not already persisted in the DB
-    while Choogle.find_by(slug: slug)
-      # when a similar slug is find (true), a new slug is generated
+    unless params["choogle"]["title"].nil?
+      @choogle = @user.choogles.new(choogle_params)
+      # we generate a random slug
       slug = SecureRandom.urlsafe_base64(5)
-    end
-
-    # our choogle slug is now our previously generated slug
-    @choogle.slug = slug
-    @proposal = Proposal.new
-    if @choogle.save
+      # we check if the slug is not already persisted in the DB
+      while Choogle.find_by(slug: slug)
+        # when a similar slug is find (true), a new slug is generated
+        slug = SecureRandom.urlsafe_base64(5)
+      end
+      # our choogle slug is now our previously generated slug
+      @choogle.slug = slug
+      @proposal = Proposal.new
+      @choogle.save
       @proposal.choogle = @choogle
+      @proposal.user = @user
+      @proposal.place = Place.last
+      @proposal.save
+    else
+      @proposal = current_user.proposals.last
+      if Place.find_by(name: params["choogle"]["proposal"]["place"]).nil?
+        # If it doesn't: let's create one!
+        @place = Place.new(name: params["choogle"]["proposal"]["place"])
+        # We are getting the API id, which will be useful in the future the query Google about our own Places Objects.
+        # Ex: if I want to fetch a Place rating on a "place" instance of Place
+      else
+        @place = Place.find_by(name: params["choogle"]["proposal"]["place"])
+      end
+      @proposal.place = @place
+      @proposal.save
+      redirect_to choogle_path(@proposal.choogle)
     end
   end
 
