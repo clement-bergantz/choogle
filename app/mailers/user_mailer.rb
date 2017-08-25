44e5c838e4bc515ce96c79acbac6d3ff20245cf3
clement bergantz <bergantz.clement@gmail.com>
2017-08-25 11:42:47 +0200

    generate model + default email + Usermailer + welcome email content

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,17 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.welcome.subject
+  #
+  def welcome(user)
+    @user = user # Instance variable => available in view
+    @choogle = @user.choogle.last
+    subject = @choogle.title
+
+    @greeting = "Hi"
+
+    mail(to: @user.email, subject: subject)
+  end
+end
