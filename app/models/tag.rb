c2a2df812cc10a08157a3cf9932026231124b61a
Romain Neuville <neuvilleromain@gmail.com>
2017-08-25 11:36:00 +0200

    bugs solved

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
 	has_many :proposal_tags
 	validates :color, presence: true
-	validates :name, presence: true, uniqueness: true
+	validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
 end
