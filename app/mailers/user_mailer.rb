5d0829d255e55c309a7f0b35d387400610e826b9
clement bergantz <bergantz.clement@gmail.com>
2017-08-25 12:04:32 +0200

    add user mailer preview

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def welcome(user)
     @user = user # Instance variable => available in view
-    @choogle = @user.choogle.last
+    @choogle = @user.choogles.last
     subject = @choogle.title
 
     @greeting = "Hi"
