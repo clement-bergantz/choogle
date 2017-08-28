3680b3dd2d96be7d4d20f7e65a1bdc0f3d6c3f4a
Romain Neuville <neuvilleromain@gmail.com>
2017-08-28 14:20:07 +0200

    Improved tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def usertags
+    usertags = []
+    current_or_guest_user.proposals.each do |proposal|
+      usertags << proposal.tags.map(&:name)
+    end
+    usertags.flatten
+  end
+
 end
