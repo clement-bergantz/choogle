cdb2f61e13edf7bb59256856eaee53865d785cb4
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-28 19:06:52 +0200

    humanized errors display

--- app/models/choogle.rb
+++ app/models/choogle.rb
@@ -6,9 +6,9 @@ class Choogle < ApplicationRecord
   has_many :places, through: :proposals
 
   validates :slug, presence: true
-  validates :title, presence: true
-  validates :happens_at, presence: true
-  validates :due_at, presence: true
+  validates :title, presence: { message: "Your Choogle should have a title!" }
+  validates :happens_at, presence: { message: "You have to add an Event date" }
+  validates :due_at, presence: { message: "You have to indicate a closing date for votes" }
 
   validate :happens_at_cannot_be_in_the_past, :due_at_cannot_be_in_the_past, :due_at_must_be_before_happens_at
 
@@ -41,7 +41,7 @@ class Choogle < ApplicationRecord
 
   def due_at_must_be_before_happens_at
     if due_at.present? && happens_at.present? && due_at >= happens_at
-      errors.add(:due_at, "should be set before Choogle date!")
+      errors.add(:closing_votes_dates, "should be set before Choogle date!")
     end
   end
 end
