57762ec0f2358e2f626525d0fd40323cd52d00fb
Romain Neuville <neuvilleromain@gmail.com>
2017-08-28 18:45:37 +0200

    comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
 
+  # This helper is used to get all tags of the current user
   def usertags
     usertags = []
     current_or_guest_user.proposals.each do |proposal|
@@ -8,6 +9,7 @@ module ApplicationHelper
     usertags.flatten
   end
 
+  # This heloer is used to sort proposals by upvotes (most in first place)
   def sort_prop_by_upvotes(proposals)
     sorted = proposals.sort_by do |proposal|
       proposal.upvotes.size
