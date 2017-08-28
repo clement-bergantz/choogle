3968fd2b193b636a3a025fb0896bc6565f346b1e
Simon Guillorit <simon.guillorit@gmail.com>
2017-08-28 14:51:31 +0200

    error displays on proposal modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   helper_method :current_or_guest_user
 
   def configure_permitted_parameters
