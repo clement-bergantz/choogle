29abc30727e332e78255a7ccd9dd12b946d47f08
Romain Neuville <neuvilleromain@gmail.com>
2017-08-28 18:58:04 +0200

    change helper usertag to User.tags, better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,6 @@
 module ApplicationHelper
 
-  # This helper is used to get all tags of the current user
-  def usertags
-    usertags = []
-    current_or_guest_user.proposals.each do |proposal|
-      usertags << proposal.tags.map(&:name)
-    end
-    usertags.flatten
-  end
-
-  # This heloer is used to sort proposals by upvotes (most in first place)
+  # This helper is used to sort proposals by upvotes (most in first place)
   def sort_prop_by_upvotes(proposals)
     sorted = proposals.sort_by do |proposal|
       proposal.upvotes.size
