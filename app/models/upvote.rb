866622bc5f7e5245c1b53c94586343f7c52930c8
Neuville Romain <neuvilleromain@gmail.com>
2017-08-29 13:28:03 +0200

    Update upvote.rb

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -3,10 +3,10 @@ class Upvote < ApplicationRecord
   belongs_to :user
   validates :user_id, uniqueness: { scope: :proposal_id }
   # Everything behind is for WS, at each save or destroy refresh on all clients
-  after_save :broadcast
-  after_destroy :broadcast
+  after_save :broadcast_upvotes
+  after_destroy :broadcast_upvotes
 
-  def broadcast
+  def broadcast_upvotes
     # looking for all clients on the broadcast and push upvotes size and proposal id
     ActionCable.server.broadcast(
     "upvote_#{proposal.choogle.slug}",
