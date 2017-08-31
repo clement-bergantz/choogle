8b1b629e654ad137808d6df66734712ab5e37e8f
Romain Neuville <neuvilleromain@gmail.com>
2017-08-31 17:18:21 +0200

    postmark domain address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_key: ENV['d7042949-b021-4146-82f1-bbf36ffd76a3'] }
-  config.action_mailer.default_url_options = { host: "http://choogle.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "https://www.choogle.xyz/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
