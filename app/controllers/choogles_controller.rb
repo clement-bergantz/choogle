99dff39a63ef0b79f902b3debede285d2c5db64c
Simon Guillorit <simon.guillorit@gmail.com>
2017-09-04 09:01:06 +0200

    comments are now displayed in the right order!

--- app/controllers/choogles_controller.rb
+++ app/controllers/choogles_controller.rb
@@ -12,17 +12,11 @@ class ChooglesController < ApplicationController
       marker.lat proposal.place.latitude
       marker.lng proposal.place.longitude
       marker.json({ :id => proposal.id })
-      # // uncomment to add a specific marker
-      # marker.picture({
-      #   "url" => view_context.image_path("marker.png"),
-      #   "width" => 64,
-      #   "height" =>64
-      # })
       marker.infowindow render_to_string(partial: "/proposals/map_box", locals: { proposal: proposal })
     end
     @user = current_or_guest_user
     @comment = Comment.new
-    @comments = Comment.where(choogle: @choogle).order('created_at DESC')
+    @comments = Comment.where(choogle: @choogle).order('created_at')
     # @place = Place.find(params[:id])
     # @place_coordinates = { lat: @place.latitude, lng: @place.longitude }
 
