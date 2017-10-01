354876033fb5ad54c08f7eb1928208ef1b5fa322
Romain Neuville <neuvilleromain@gmail.com>
2017-10-01 12:43:55 +0200

    timer ok

--- app/models/choogle.rb
+++ app/models/choogle.rb
@@ -17,6 +17,11 @@ class Choogle < ApplicationRecord
 
   before_validation :generate_slug, on: :create
 
+  def due_at_tz
+     tz = TZInfo::Timezone.get(user.timecode)
+     tz.local_to_utc(due_at)
+  end
+
   def generate_slug
     slug = SecureRandom.urlsafe_base64(5)
     while Choogle.find_by(slug: slug)
@@ -48,7 +53,4 @@ class Choogle < ApplicationRecord
     end
   end
 
-  # def winning_proposal
-  #   proposals.
-  # end
 end
